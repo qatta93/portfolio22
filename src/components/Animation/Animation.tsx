0582df2eac25d230c4732df971e23d653fa918cb
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-11 13:10:53 +0200

    remove animation

--- src/components/Animation/Animation.tsx
+++ src/components/Animation/Animation.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactBodymovin from 'react-bodymovin'
-import animationData from './data.json'
 
 export const Animation = () => {
-  const animation = {
-    loop: true,
-    autoplay: true,
-    prerender: true,
-    animationData: animationData
-  }
   return (
     <>
       <div>Animation</div>
-      <ReactBodymovin options={animation} />
     </>
   )
 }
