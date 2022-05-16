f0a4898dbbe2f991aaa4cb5455a89a6a9101f330
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-16 11:22:50 +0200

    mobile responsive

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Animation } from '../components/Animation/Animation';
 import { Footer } from './Footer';
 import styles from '../../public/styles/Intro.module.css';
 
@@ -7,13 +6,13 @@ export const Intro = () => {
   return (
     <>
       <section className={styles.intro}>
-        <div className={styles.intro__header}>
-          <h1 className={styles.intro__hello}>Hello,</h1>
-          <h1 className={styles.intro__name}>I'm Patrycja Panasiuk</h1>
-          <h2 className={styles.intro__dev}>frontend and UX/UI enthusiast</h2>
-        </div>
-        <div className={styles.intro__animation}>
-          <Animation />
+        <div className={styles.intro__wrapper}>
+          <div className={styles.intro__header}>
+            <h1 className={styles.intro__hello}>Hello,</h1>
+            <h1 className={styles.intro__name}>I'm Patrycja Panasiuk</h1>
+            <h2 className={styles.intro__dev}>frontend and UX/UI enthusiast</h2>
+            <a href='about-me' className={styles.intro__btn}>Get to know me!</a>
+          </div>
         </div>
         <div className={styles.intro__footer}><Footer /></div>
       </section>
