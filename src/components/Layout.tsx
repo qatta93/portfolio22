1a761129dbb41c2f0e5ed5642bcae31b683b4bed
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-18 15:16:17 +0200

    handle width changes fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from './Navbar';
 import { MenuMobile } from './MenuMobile';
 import styles from '../../public/styles/Layout.module.css';
 
 export const Layout = ({ children }:React.PropsWithChildren<{}>) => {
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [width, setWidth] = React.useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+  const [vh, setVh] = React.useState(0);
 
   const isTablet = 767 < width;
   const isPhone = 520 > width;
+  const handleResize = () => setWidth(window.innerWidth);
 
   if (typeof window !== "undefined") {
-    const [vh, setVh] = React.useState(0);
-    React.useEffect(() => {
+    useEffect(() => {
       setVh(window.innerHeight * 0.01);
-      setWidth(window.innerWidth);
-    }, [window.innerWidth]);
+      window.addEventListener('resize', handleResize);
+      () => window.removeEventListener('resize', handleResize);
       if(isPhone && width !== 0){
         document.documentElement.style.setProperty('--vh', `${vh}px`);
       }
+    }, [handleResize]);
   }
-
-  console.log(width);
 
   return (
     <div className={styles.wrapper}>
