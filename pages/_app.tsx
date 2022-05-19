45ec1083ca08a1fd50c2b20837bc44d9ae618e14
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-19 11:14:36 +0200

    add favion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import App, { AppContext, AppProps } from 'next/app';
 import { Layout } from "../src/components/Layout";
+import Head from 'next/head';
 import "/public/styles/styles.css";
 
 const MyApp = ({ Component, pageProps }:AppProps) => {
   return (
+    <>
+    <Head>
+      <link rel="shortcut icon" href="/favicon.png" />
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 };
 
