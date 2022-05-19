01f1874e0cc5780a3bd0ee9d8cf376335b03088a
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-19 12:27:07 +0200

    navbar resize bug fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,25 +6,20 @@ import styles from '../../public/styles/Layout.module.css';
 export const Layout = ({ children }:React.PropsWithChildren<{}>) => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [vh, setVh] = React.useState(0);
+  const [width, setWidth] = useState<number>(0);
   
   const handleResize = () => setWidth(window.innerWidth);
-  const [width, setWidth] = useState<number>(0);
-  const isTablet = (767 < width && width !==0);
-  
-  if (typeof window !== "undefined" && width === 0){
-    const defaultWidth = window.innerHeight;
-    setWidth(defaultWidth);
-  }
+  const isTablet = 767 < width;
 
   if (typeof window !== "undefined") {
     useEffect(() => {
       setVh(window.innerHeight * 0.01);
+      setWidth(window.innerWidth);
       window.addEventListener('resize', handleResize);
       () => window.removeEventListener('resize', handleResize);
-    }, [handleResize]);
+    }, [width]);
     document.documentElement.style.setProperty('--vh', `${vh}px`)
   }
-
 
   return (
     <div className={styles.wrapper}>
