4ce4f33d571ed8b0f181af940fae8e4ee682ec1f
qatta93 <panasiuk.patrycja@gmail.com>
2023-01-23 14:48:18 +0100

    update projects info

--- src/components/Projects/ActiveProject/index.tsx
+++ src/components/Projects/ActiveProject/index.tsx
@@ -13,10 +13,11 @@ export const ActiveProject = ({ project, setActiveProject }:ActiveProjectProps) 
       <div className={styles.activeProject__wrapper}>
         <h1 className={styles.activeProject__name}>{project.name}</h1>
         <h2 className={styles.activeProject__tech}>{technologies}</h2>
+        <h3 className={styles.activeProject__role}>{project.role}</h3>
         <p className={styles.activeProject__desc}>{project.description}</p>
         <div className={styles.activeProject__links}>
           <h3 className={styles.activeProject__header}>LINKS:</h3>
-          {project.links.map((link, i) => <a key={link} className={styles.activeProject__link} href={project.url[i]}>{link}</a>)}
+          {project.links.map((link, i) => <a key={link} className={styles.activeProject__link} href={project.url[i]} target="_blank">{link}</a>)}
         </div>
       </div>
     </section>
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export interface ProjectProps {
+  role: ReactNode;
   id: number,
   name: string,
   tech: Array<string>,
