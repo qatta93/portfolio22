0ce9d89e3410f6290b4c25268b59c15790ee1c3f
qatta93 <panasiuk.patrycja@gmail.com>
2023-02-17 11:41:23 +0100

    update lottie animations

--- src/components/Projects/MotionDesign/index.tsx
+++ src/components/Projects/MotionDesign/index.tsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import styles from '../../../../public/styles/MotionDesign.module.css';
+import Lottie from "lottie-react";
+import roomAnimationData from "./room.json";
+import girlAnimationData from "./girl.json";
 
 export const MotionDesign = () => {
   return (
     <div>
+      <Lottie animationData={roomAnimationData} loop={true} />
+      <Lottie animationData={girlAnimationData} loop={true} className={styles.lottie}/>
       <iframe
         title="Car in motion video player"
         src="/videos/carMobile.mp4"
         sandbox=""
         allowFullScreen
         className={styles.videoFrame__car}
-        loading="lazy"
-        >
-      </iframe>
-      <iframe
-        title="Car in motion video player"
-        src="/videos/lottie.mp4"
-        sandbox=""
-        allowFullScreen
-        className={styles.videoFrame__lottie}
         loading="lazy"
         >
       </iframe>
