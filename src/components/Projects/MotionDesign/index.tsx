db3f2ea0a670fc4517b4ca23102bf164d1de8ad0
qatta93 <panasiuk.patrycja@gmail.com>
2023-02-17 19:41:29 +0100

    add motion design project

--- src/components/Projects/MotionDesign/index.tsx
+++ src/components/Projects/MotionDesign/index.tsx
@@ -3,12 +3,14 @@ import styles from '../../../../public/styles/MotionDesign.module.css';
 import Lottie from "lottie-react";
 import roomAnimationData from "./room.json";
 import girlAnimationData from "./girl.json";
+import landscapeAnimationData from "./landscape.json";
 
 export const MotionDesign = () => {
   return (
     <div>
       <Lottie animationData={roomAnimationData} loop={true} />
-      <Lottie animationData={girlAnimationData} loop={true} className={styles.lottie}/>
+      <Lottie animationData={landscapeAnimationData} loop={true} className={styles.lottie}/>
+      <Lottie animationData={girlAnimationData} loop={true}/>
       <iframe
         title="Car in motion video player"
         src="/videos/carMobile.mp4"
