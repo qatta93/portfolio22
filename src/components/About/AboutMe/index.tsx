8af581230c05f32a7f2e4281bf6ba6b7dc7c6b7c
qatta93 <panasiuk.patrycja@gmail.com>
2023-08-16 20:27:17 +0200

    update experience

--- src/components/About/AboutMe/index.tsx
+++ src/components/About/AboutMe/index.tsx
@@ -22,26 +22,48 @@ export const AboutMe = () => {
         <article className={styles.aboutComp__timeline}>
           <h1 className={styles.aboutComp__timeline__header}>TIMELINE:</h1>
           <VerticalTimeline>
-            <VerticalTimelineElement
+          <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2022 - 2023"
+              date="2023 - present"
               iconStyle={{ background: 'var(--yellow)' }}
             >
-              <h3 className="vertical-timeline-element-title">UX Designer / Frontend Developer</h3>
-              <h4 className="vertical-timeline-element-subtitle">{`Change Equity`}</h4>
-              <h4 className="vertical-timeline-element-subtitle">Stockholm, Sweden (remote)</h4>
-              <p>
-              The project name is Noterat.io. It's a user-friendly application that distributes and analyzes the available data on the Swedish Parliaments (Riksdag) API and website. Our aim is to create an informative and neutral platform where the users can easily access and control specific documents and political activity without ending up in filter bubbles. I have been creating a design in Figma and helping other developers with coding features in React and TypeScript.
-              </p>
+              <h3 className="vertical-timeline-element-title">Frontend Developer</h3>
+              <h4 className="vertical-timeline-element-subtitle">{`Adlife`}</h4>
+              <h4 className="vertical-timeline-element-subtitle">Warsaw, Poland</h4>
+              <ul>
+                <li>creating Web Applications from scratch and developing existing ones with new functionalities using React and Redux in a scalable way</li>
+                <li>collaborating with Project Managers, BE developers, BA, UX/UI (auditing UX design from a tech perspective and giving insights), and consultation with clients to deliver user-centered and highly performant Web App</li>
+                <li>integrating B2C websites with headless CMS to allow content editors easily maintain a website</li>
+                <li>actively participating in scrum meetings (backlog refinement, sprint planning, retro)</li>
+                <li>considering SEO practices to enhance the website's search engine visibility</li>
+              </ul>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               date="2021 - 2023"
               iconStyle={{ background: 'var(--yellow)' }}
             >
+              <h3 className="vertical-timeline-element-title">Software Developer Consultant</h3>
+              <h4 className="vertical-timeline-element-subtitle">{`SALT`}</h4>
+              <h4 className="vertical-timeline-element-subtitle">Oslo, Norway (remote)</h4>
+              <ul>
+                <li>identifying and prioritizing the most crucial frontend features for the MVP based on user needs and business goals</li>
+                <li>implementing complex design mockups and wireframes using Storybook, maintaining design fidelity and usability</li>
+                <li>utilizing browser developer tools to diagnose and debug frontend issues, optimizing performance, and troubleshooting layout problems</li>
+                <li>developing interactive elements using JavaScript and React, enhancing user engagement through animations, transitions, and real-time updates</li>
+                <li>integrating with backend services by consuming RESTful APIs to fetch and display dynamic content</li>
+                <li>participating in UI design and interactive prototypes to visualize and iterate on user interfaces, ensuring usability and efficiency before full development</li>
+                <li>prioritizing responsive design across a range of devices (mobile first)</li>
+              </ul>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="2021 - 2022"
+              iconStyle={{ background: 'var(--yellow)' }}
+            >
               <h3 className="vertical-timeline-element-title">Full-Stack JavaScript Developer</h3>
               <h4 className="vertical-timeline-element-subtitle">{`</salt>`}</h4>
-              <h4 className="vertical-timeline-element-subtitle">Oslo, Norway</h4>
+              <h4 className="vertical-timeline-element-subtitle">Stockholm, Sweden</h4>
               <p>
               Technical skills include JavaScript, React, Typescript, Node.js, Express, HTML5, CSS3, Databases incl. MongoDB and PostgreSQL, UX/UI and general development tools such as Git, REST, Docker, Kubernetes, Redux, GraphQL, AWS, TDD and CI/CD.
               </p>
@@ -54,45 +76,38 @@ export const AboutMe = () => {
               <h3 className="vertical-timeline-element-title">Leadership training workshop</h3>
               <h4 className="vertical-timeline-element-subtitle">online</h4>
               <p>
-              EU program - practical course that increases soft skills and improves teamwork (100h).
+              The entire program took 100 hours. It was divided into 5 practical modules: leader person, leadership communication, team leader, leadership tools and design tools. 
+              The course was designed to increase soft skills and working in a team.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2021"
+              date="2019 - 2022"
               iconStyle={{ background: '#efeff0' }}
             >
-              <h3 className="vertical-timeline-element-title">Civil Engineer</h3>
+              <h3 className="vertical-timeline-element-title">Architect, Product Owner</h3>
               <h4 className="vertical-timeline-element-subtitle">self-employed</h4>
               <h4 className="vertical-timeline-element-subtitle">Warsaw, Poland</h4>
-              <p>
-                Architectural and structural engineering consulting.
-              </p>
+              <ul>
+                <li>creating a brand of a company that sells prefabricated houses</li>
+                <li>researching the market, looking for new solutions and taking into account the needs of customers</li>
+                <li>constantly devising automation solutions (for instance - an online configurator in order to help clients with cost estimation)</li>
+                <li>creating a website from scratch - from logo, graphic design to programming (Adobe XD, HTML, CSS, JavaScript, React)</li>
+                <li>collaborating with clients and multidisciplinary teams</li>
+              </ul>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2020-2021"
-              iconStyle={{ background: '#efeff0' }}
-            >
-              <h3 className="vertical-timeline-element-title">Architect / Brand manager</h3>
-              <h4 className="vertical-timeline-element-subtitle">Akrobud</h4>
-              <h4 className="vertical-timeline-element-subtitle">Warsaw, Poland</h4>
-              <p>
-              I created a brand of a company selling prefabricated houses. I did everything from scratch - the name, logo, projects, business model etc. I was researching a market, looking for new solutions and trying to play the role of my potential clients. 
-              I sold 4 houses after a few months of creating the company. I realized how important is to present information in a simple and intuitive way (UX/UI). I designed and coded a website (HTML, CSS, JS, PHP, ADOBE XD). 
-              I was planning to do 3D configurator in the future to make the website more automated and user friendly.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2015-2020"
+              date="2015-2029"
               iconStyle={{ background: '#efeff0' }}
             >
               <h3 className="vertical-timeline-element-title">Architect / Civil Engineer</h3>
-              <h4 className="vertical-timeline-element-subtitle">Germany, The UK, Poland</h4>
-              <p>
-                Working as an engineer in international projects.
-              </p>
+              <h4 className="vertical-timeline-element-subtitle">New Zealand, England, Germany, Poland</h4>
+              <ul>
+                <li>creating mockups and 3D models with an aesthetic sense</li>
+                <li>thinking analytically and solving engineering problems</li>
+                <li>working with multicultural and multidisciplinary teams</li>
+              </ul>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
