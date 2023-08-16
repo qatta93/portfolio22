476052b6bb781c2555ab7a28d49f958ebc08a10c
qatta93 <panasiuk.patrycja@gmail.com>
2023-08-16 20:34:38 +0200

    style experience section

--- src/components/About/AboutMe/index.tsx
+++ src/components/About/AboutMe/index.tsx
@@ -30,7 +30,7 @@ export const AboutMe = () => {
               <h3 className="vertical-timeline-element-title">Frontend Developer</h3>
               <h4 className="vertical-timeline-element-subtitle">{`Adlife`}</h4>
               <h4 className="vertical-timeline-element-subtitle">Warsaw, Poland</h4>
-              <ul>
+              <ul className={styles.experience_list}>
                 <li>creating Web Applications from scratch and developing existing ones with new functionalities using React and Redux in a scalable way</li>
                 <li>collaborating with Project Managers, BE developers, BA, UX/UI (auditing UX design from a tech perspective and giving insights), and consultation with clients to deliver user-centered and highly performant Web App</li>
                 <li>integrating B2C websites with headless CMS to allow content editors easily maintain a website</li>
@@ -46,7 +46,7 @@ export const AboutMe = () => {
               <h3 className="vertical-timeline-element-title">Software Developer Consultant</h3>
               <h4 className="vertical-timeline-element-subtitle">{`SALT`}</h4>
               <h4 className="vertical-timeline-element-subtitle">Oslo, Norway (remote)</h4>
-              <ul>
+              <ul className={styles.experience_list}>
                 <li>identifying and prioritizing the most crucial frontend features for the MVP based on user needs and business goals</li>
                 <li>implementing complex design mockups and wireframes using Storybook, maintaining design fidelity and usability</li>
                 <li>utilizing browser developer tools to diagnose and debug frontend issues, optimizing performance, and troubleshooting layout problems</li>
@@ -88,7 +88,7 @@ export const AboutMe = () => {
               <h3 className="vertical-timeline-element-title">Architect, Product Owner</h3>
               <h4 className="vertical-timeline-element-subtitle">self-employed</h4>
               <h4 className="vertical-timeline-element-subtitle">Warsaw, Poland</h4>
-              <ul>
+              <ul className={styles.experience_list}>
                 <li>creating a brand of a company that sells prefabricated houses</li>
                 <li>researching the market, looking for new solutions and taking into account the needs of customers</li>
                 <li>constantly devising automation solutions (for instance - an online configurator in order to help clients with cost estimation)</li>
@@ -103,7 +103,7 @@ export const AboutMe = () => {
             >
               <h3 className="vertical-timeline-element-title">Architect / Civil Engineer</h3>
               <h4 className="vertical-timeline-element-subtitle">New Zealand, England, Germany, Poland</h4>
-              <ul>
+              <ul className={styles.experience_list}>
                 <li>creating mockups and 3D models with an aesthetic sense</li>
                 <li>thinking analytically and solving engineering problems</li>
                 <li>working with multicultural and multidisciplinary teams</li>
